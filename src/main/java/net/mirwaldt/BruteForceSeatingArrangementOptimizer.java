7cacd9e8db1f2b69681c63ce84a5062377358fdb
mmirwaldt <michael.mirwaldt@gmail.com>
2021-02-26 22:03:03 +0100

    brute force algorithm developed and tested

--- src/main/java/net/mirwaldt/BruteForceSeatingArrangementOptimizer.java
+++ src/main/java/net/mirwaldt/BruteForceSeatingArrangementOptimizer.java
@@ -1,15 +1,70 @@
 package net.mirwaldt;
 
-import java.util.Collections;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class BruteForceSeatingArrangementOptimizer implements SeatingArrangementOptimizer {
+    private final Map<List<String>, Integer> preferences = new HashMap<>();
+
     @Override
     public void addPreference(String name, String neighbour, int happinessUnits) {
-
+        preferences.put(Arrays.asList(name, neighbour), happinessUnits);
     }
 
     @Override
     public SeatingArrangement findOptimalSeatingArrangement() {
-        return new SeatingArrangement(Collections.emptyList(), 0);
+        final SortedSet<String> allNames = preferences.keySet().stream().
+                flatMap(Collection::stream).collect(Collectors.toCollection(TreeSet::new));
+        if (2 <= allNames.size()) {
+            final Map<List<String>, Integer> seatingByHappiness = seat(allNames, Collections.emptyList());
+            Map.Entry<List<String>, Integer> seatingWithMaxHappiness =
+                    seatingByHappiness.entrySet().stream()
+                            .max(Comparator.comparingInt(Map.Entry::getValue)).get();
+
+            return new SeatingArrangement(seatingWithMaxHappiness.getKey(), seatingWithMaxHappiness.getValue());
+        } else {
+            throw new IllegalStateException("At least two persons are needed for a seating!");
+        }
+    }
+
+    private Map<List<String>, Integer> seat(SortedSet<String> names, List<String> seating) {
+        if (names.isEmpty()) {
+            return Map.of(seating, calculateHappiness(seating));
+        } else {
+            final Map<List<String>, Integer> seatingsByHappiness = new LinkedHashMap<>();
+            for (String name : names) {
+                SortedSet<String> restNames = new TreeSet<>(names);
+                restNames.remove(name);
+                List<String> newSeating = new ArrayList<>(seating);
+                newSeating.add(name);
+                seatingsByHappiness.putAll(seat(restNames, newSeating));
+            }
+            return seatingsByHappiness;
+        }
+    }
+
+    int calculateHappiness(List<String> seating) {
+        int totalHappiness = 0;
+        for (int i = 0; i < seating.size(); i++) {
+            int fromIndex = i % seating.size();
+            int toIndex = (i + 1) % seating.size();
+            final List<String> seatingPair = new ArrayList<>(); // we cannot use sublist() because we use reverse() later!
+            seatingPair.add(seating.get(fromIndex));
+            seatingPair.add(seating.get(toIndex));
+
+            Integer happinessForLeft = preferences.get(seatingPair);
+            if (happinessForLeft == null) {
+                throw new IllegalStateException("Cannot find happiness for seating pair " + seatingPair);
+            }
+            totalHappiness += happinessForLeft;
+
+            Collections.reverse(seatingPair);
+            Integer happinessForRight = preferences.get(seatingPair);
+            if (happinessForRight == null) {
+                throw new IllegalStateException("Cannot find happiness for seating pair " + seatingPair);
+            }
+            totalHappiness += happinessForRight;
+        }
+        return totalHappiness;
     }
 }
--- src/test/java/net/mirwaldt/SeatingArrangementOptimizerTest.java
+++ src/test/java/net/mirwaldt/SeatingArrangementOptimizerTest.java
@@ -1,5 +1,6 @@
 package net.mirwaldt;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -16,21 +17,37 @@ public class SeatingArrangementOptimizerTest {
 
     @ParameterizedTest
     @MethodSource("seatingArrangementOptimizer")
-    void test_example(SeatingArrangementOptimizer seatingArrangementOptimizer) {
-        /*
-            Alice would gain 54 happiness units by sitting next to Bob.
-            Alice would lose 79 happiness units by sitting next to Carol.
-            Alice would lose 2 happiness units by sitting next to David.
-            Bob would gain 83 happiness units by sitting next to Alice.
-            Bob would lose 7 happiness units by sitting next to Carol.
-            Bob would lose 63 happiness units by sitting next to David.
-            Carol would lose 62 happiness units by sitting next to Alice.
-            Carol would gain 60 happiness units by sitting next to Bob.
-            Carol would gain 55 happiness units by sitting next to David.
-            David would gain 46 happiness units by sitting next to Alice.
-            David would lose 7 happiness units by sitting next to Bob.
-            David would gain 41 happiness units by sitting next to Carol.
-         */
+    void test_findOptimalSeatingArrangement(SeatingArrangementOptimizer seatingArrangementOptimizer) {
+        addPreferences(seatingArrangementOptimizer);
+        final SeatingArrangement seatingArrangement = seatingArrangementOptimizer.findOptimalSeatingArrangement();
+        assertEquals(Arrays.asList("Alice", "Bob", "Carol", "David"), seatingArrangement.getSeating());
+        assertEquals(330, seatingArrangement.getTotalHappinessUnits());
+    }
+
+    @Test
+    void test_example() {
+        BruteForceSeatingArrangementOptimizer seatingArrangementOptimizer =
+                new BruteForceSeatingArrangementOptimizer();
+        addPreferences(seatingArrangementOptimizer);
+        assertEquals(330, seatingArrangementOptimizer.calculateHappiness(
+                Arrays.asList("Alice", "Bob", "Carol", "David")));
+    }
+
+    private void addPreferences(SeatingArrangementOptimizer seatingArrangementOptimizer) {
+    /*
+        Alice would gain 54 happiness units by sitting next to Bob.
+        Alice would lose 79 happiness units by sitting next to Carol.
+        Alice would lose 2 happiness units by sitting next to David.
+        Bob would gain 83 happiness units by sitting next to Alice.
+        Bob would lose 7 happiness units by sitting next to Carol.
+        Bob would lose 63 happiness units by sitting next to David.
+        Carol would lose 62 happiness units by sitting next to Alice.
+        Carol would gain 60 happiness units by sitting next to Bob.
+        Carol would gain 55 happiness units by sitting next to David.
+        David would gain 46 happiness units by sitting next to Alice.
+        David would lose 7 happiness units by sitting next to Bob.
+        David would gain 41 happiness units by sitting next to Carol.
+     */
         seatingArrangementOptimizer.addPreference("Alice", "Bob", 54);
         seatingArrangementOptimizer.addPreference("Alice", "Carol", -79);
         seatingArrangementOptimizer.addPreference("Alice", "David", -2);
@@ -43,9 +60,6 @@ public class SeatingArrangementOptimizerTest {
         seatingArrangementOptimizer.addPreference("David", "Alice", 46);
         seatingArrangementOptimizer.addPreference("David", "Bob", -7);
         seatingArrangementOptimizer.addPreference("David", "Carol", 41);
-        final SeatingArrangement seatingArrangement = seatingArrangementOptimizer.findOptimalSeatingArrangement();
-        assertEquals(Arrays.asList("Alice", "Bob", "Carol", "David"), seatingArrangement.getSeating());
-        assertEquals(330, seatingArrangement.getTotalHappinessUnits());
     }
 
 }
