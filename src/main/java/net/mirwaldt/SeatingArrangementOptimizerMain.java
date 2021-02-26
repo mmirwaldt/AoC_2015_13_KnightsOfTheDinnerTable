c86bcb427f96d6df50bb90e878aeab6256d17955
mmirwaldt <michael.mirwaldt@gmail.com>
2021-02-26 22:17:05 +0100

    part two of the puzzle solved

--- src/main/java/net/mirwaldt/SeatingArrangementOptimizerMain.java
+++ src/main/java/net/mirwaldt/SeatingArrangementOptimizerMain.java
@@ -5,11 +5,45 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class SeatingArrangementOptimizerMain {
     public static void main(String[] args) throws IOException {
         final List<String> lines = Files.readAllLines(Path.of("input.txt"), StandardCharsets.US_ASCII);
-        final SeatingArrangementOptimizer seatingArrangementOptimizer = new BruteForceSeatingArrangementOptimizer();
+
+        final SeatingArrangementOptimizer seatingArrangementOptimizerForPartOne =
+                new BruteForceSeatingArrangementOptimizer();
+        addPreferences(lines, seatingArrangementOptimizerForPartOne);
+        final SeatingArrangement seatingArrangementOfPartOne =
+                seatingArrangementOptimizerForPartOne.findOptimalSeatingArrangement();
+
+        // [Alice, Bob, Frank, Carol, Eric, David, George, Mallory] : 733
+        System.out.println(seatingArrangementOfPartOne.getSeating() + " : "
+                + seatingArrangementOfPartOne.getTotalHappinessUnits());
+
+
+
+        final SeatingArrangementOptimizer seatingArrangementOptimizerForPartTwo =
+                new BruteForceSeatingArrangementOptimizer();
+        final SortedSet<String> allNames = addPreferences(lines, seatingArrangementOptimizerForPartTwo);
+        final String myName = "Michael";
+        for (String name : allNames) {
+            seatingArrangementOptimizerForPartTwo.addPreference(myName, name, 0);
+            seatingArrangementOptimizerForPartTwo.addPreference(name, myName, 0);
+        }
+
+        final SeatingArrangement seatingArrangementOfPartTwo =
+                seatingArrangementOptimizerForPartTwo.findOptimalSeatingArrangement();
+
+        // [Alice, Bob, Frank, Carol, Eric, David, George, Michael, Mallory] : 725
+        System.out.println(seatingArrangementOfPartTwo.getSeating() + " : "
+                + seatingArrangementOfPartTwo.getTotalHappinessUnits());
+    }
+
+    private static SortedSet<String> addPreferences(
+            List<String> lines, SeatingArrangementOptimizer seatingArrangementOptimizer) {
+        final SortedSet<String> allNames = new TreeSet<>();
         for (String line : lines) {
             final String[] tokens =  line.split(" ");
             final String name = tokens[0];
@@ -21,10 +55,9 @@ public class SeatingArrangementOptimizerMain {
                 happinessUnits = Integer.parseInt(tokens[3]);
             }
             seatingArrangementOptimizer.addPreference(name, neighbour, happinessUnits);
+            allNames.add(name);
+            allNames.add(neighbour);
         }
-        final SeatingArrangement seatingArrangement = seatingArrangementOptimizer.findOptimalSeatingArrangement();
-
-        // [Alice, Bob, Frank, Carol, Eric, David, George, Mallory] : 733
-        System.out.println(seatingArrangement.getSeating() + " : " + seatingArrangement.getTotalHappinessUnits());
+        return allNames;
     }
 }
